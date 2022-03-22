4b985c758e632ef8aa3e15c597f4477eb518eabe
Yuşe Yusuf Kılıç <79745654+yuseyusufkilic@users.noreply.github.com>
2022-03-22 12:47:20 +0300

    Basic ASPNETCore MVC Example
    Basic example has been ran. Fundamentals of MVC learned and performed.

---
+++ basicASPNETCoreMVC/basicASPNETCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using basicASPNETCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basicASPNETCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Yuse Yusuf";            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Invitation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Invitation(Participant participant,string checkPart )
+        {
+            if (checkPart=="participate")
+            {
+                return View("Thanks");
+
+            }
+            else if(checkPart=="notparticipate")
+            {
+                return View("Sad");
+
+            }
+            return View();
+        }
+    }
+}
---
+++ basicASPNETCoreMVC/basicASPNETCoreMVC/Models/Participant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basicASPNETCoreMVC.Models
+{
+    public class Participant
+    {
+        [Required(ErrorMessage ="Mail alanı boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage ="E-Posta hatalı girildi.")]
+        public string Email { get; set; }
+
+        [MinLength(2,ErrorMessage ="İsim çok kısa.")]
+        [Required(ErrorMessage = "İsim alanı boş bırakılamaz.")]
+        public string Name { get; set; }
+    }
+}
